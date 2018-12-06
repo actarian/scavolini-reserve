3d43004fbaa5a524f747d8fdb535476c67733faa
actarian <lzampetti@gmail.com>
2018-12-06 17:49:26 +0100

    scavolini-reserve

--- app/controllers/reserve/confirm.controller.js
+++ app/controllers/reserve/confirm.controller.js
@@ -13,40 +13,13 @@
 			state: state,
 			store: store,
 			booking: booking,
-			onSelectCategory: onSelectCategory,
-			onSelectProduct: onSelectProduct,
+			onBack: onBack,
 			onSubmit: onSubmit,
 		};
 
 		Object.assign($scope, publics);
 
 		state.ready();
-
-		function onSelectCategory() {
-			console.log('ReserveController.onSelectCategory');
-			$scope.$root.addModal('categoriesModal', booking.categories).then(function resolve(data) {
-				console.log('categoriesModal.resolve', data);
-				booking.model.category = data;
-				booking.model.products = null;
-
-			}, function reject(data) {
-				console.log('categoriesModal.reject', data);
-
-			});
-		}
-
-		function onSelectProduct() {
-			console.log('ReserveController.onSelectProduct');
-			var products = booking.getProductsByCategory(booking.model.category);
-			$scope.$root.addModal('productsModal', products).then(function resolve(data) {
-				console.log('productsModal.resolve', data);
-				booking.model.products = data;
-
-			}, function reject(data) {
-				console.log('productsModal.reject', data);
-
-			});
-		}
 
 		function onBack() {
 			$location.path('/reserve/' + store.id + '/planner');
